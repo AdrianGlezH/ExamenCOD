3d61b0dc5a3ff626a7a4e979fbea81bc4586a081
Adrián González Hermida <agonzalezhermida@danielcastelao.org>
2022-03-16 14:30:47 +0100

    Squashed commit of the following:
    commit acefeea7eecdd24db7fdf8ce051f143aad24eb63
    Author: Adrián González Hermida <agonzalezhermida@danielcastelao.org>
    Date:   Wed Mar 16 14:30:07 2022 +0100
    
        Commit después del merge squash
    
    commit fa035b0af73073930d5d51202191b560eb0ff092
    Author: Adrián González Hermida <agonzalezhermida@danielcastelao.org>
    Date:   Wed Mar 16 14:28:14 2022 +0100
    
        Commit con codigo y un comentario

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,24 @@
-public class main {
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+/*Implementamos todas las librerias de discord, para que funcionen métodos*/
+public class main { //Código que hace que funcione el bot
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        final String token = args[0];
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
 
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
     }
 }
